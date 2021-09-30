d97d172ec5a8cb9ca3685688f5f8b1c7825fbe50
av-ugolkov <makedonskiy07@gmail.com>
2021-09-30 07:45:34 +0300

    в примере оставляем только heatmap_month

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:heatmap_calendar/heatmap_calendar.dart';
+import 'package:heatmap_calendar/heatmap_calendar_month/heatmap_calendar_month.dart';
 import 'package:heatmap_calendar/time_utils.dart';
 
 void main() {
@@ -39,30 +40,42 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: HeatMapCalendar(
-          startDate: DateTime.now().subtract(const Duration(days: 222)),
-          finishDate: DateTime.now().add(const Duration(days: 80)),
-          input: {
-            DateUtils.dateOnly(DateTime.now().subtract(const Duration(days: 3))): 5,
-            DateUtils.dateOnly(DateTime.now().subtract(const Duration(days: 2))): 35,
-            DateUtils.dateOnly(DateTime.now().subtract(const Duration(days: 1))): 14,
-            DateUtils.dateOnly(DateTime.now()): 5,
-          },
-          colorThresholds: {
-            1: Colors.green.shade100,
-            10: Colors.green.shade300,
-            30: Colors.green.shade500
-          },
-          weekDaysLabels: TimeUtils.defaultWeekLabels,
-          monthsLabels: TimeUtils.defaultMonthsLabels,
-          squareSize: 22.0,
-          textOpacity: 1,
-          showDateLabel: false,
-          labelTextColor: Colors.blueGrey,
-          textStyleDateText: const TextStyle(color: Colors.white),
-          onTapHeatMapDay: (tapDate) {
-            log(tapDate.toString());
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            /*HeatMapCalendar(
+              startDate: DateTime.now().subtract(const Duration(days: 222)),
+              finishDate: DateTime.now().add(const Duration(days: 80)),
+              input: {
+                DateUtils.dateOnly(
+                    DateTime.now().subtract(const Duration(days: 3))): 5,
+                DateUtils.dateOnly(
+                    DateTime.now().subtract(const Duration(days: 2))): 35,
+                DateUtils.dateOnly(
+                    DateTime.now().subtract(const Duration(days: 1))): 14,
+                DateUtils.dateOnly(DateTime.now()): 5,
+              },
+              colorThresholds: {
+                1: Colors.green.shade100,
+                10: Colors.green.shade300,
+                30: Colors.green.shade500
+              },
+              weekDaysLabels: TimeUtils.defaultWeekLabels,
+              monthsLabels: TimeUtils.defaultMonthsLabels,
+              squareSize: 22.0,
+              textOpacity: 1,
+              showDateLabel: false,
+              labelTextColor: Colors.blueGrey,
+              textStyleDateText: const TextStyle(color: Colors.white),
+              onTapHeatMapDay: (tapDate) {
+                log(tapDate.toString());
+              },
+            ),*/
+            HeatMapCalendarMonth(
+              startDate: DateTime(2021, 9, 1),
+              finishDate: DateTime(2022, 1, 1),
+            ),
+          ],
         ),
       ),
     );
