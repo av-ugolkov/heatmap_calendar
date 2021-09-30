f5f256218ed171aa941122e4d44f584a1a7d3e6a
av-ugolkov <makedonskiy07@gmail.com>
2021-09-30 21:40:38 +0300

    выделение дат

--- lib/heatmap_calendar_month/heatmap_day.dart
+++ lib/heatmap_calendar_month/heatmap_day.dart
@@ -2,16 +2,35 @@ import 'package:flutter/material.dart';
 
 class HeatMapDay extends StatelessWidget {
   final DateTime currentDate;
+  final double width;
+  final double height;
+  final double opacity;
 
   const HeatMapDay({
     Key? key,
     required this.currentDate,
+    required this.width,
+    required this.height,
+    required this.opacity,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(currentDate.day.toString()),
+    return Opacity(
+      opacity: opacity,
+      child: Container(
+        alignment: Alignment.center,
+        height: height,
+        margin: const EdgeInsets.all(2.0),
+        child: Container(
+          alignment: Alignment.center,
+          width: width,
+          color: Colors.grey,
+          child: Center(
+            child: Text(currentDate.day.toString()),
+          ),
+        ),
+      ),
     );
   }
 }
