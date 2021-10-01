89fa42bed338d63f62b692493dbd262d75996f22
av-ugolkov <makedonskiy07@gmail.com>
2021-10-01 09:27:32 +0300

    внешний вид и выбо дат

--- lib/heatmap_calendar_month/heatmap_calendar_month.dart
+++ lib/heatmap_calendar_month/heatmap_calendar_month.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:heatmap_calendar/heatmap_calendar_month/heatmap_day.dart';
+import 'package:heatmap_calendar/heatmap_calendar_month/heatmap_month.dart';
+import 'package:heatmap_calendar/time_utils.dart';
 
 class HeatMapCalendarMonth extends StatefulWidget {
   static const double margin = 4.0;
 
   final DateTime startDate;
   final DateTime finishDate;
-  final double heightCell;
+
+  final Map<DateTime, int> input;
+  final Map<int, Color> colorThresholds;
+
+  final List<String> monthsLabels;
+  final List<String> weekDaysLabels;
+
+  final Function(DateTime)? onTapHeatMapDay;
+
+  final double cellHeight;
   final double disableOpacity;
 
   final bool mondayFirstDayWeek;
+  final int marginHorizontal;
 
   const HeatMapCalendarMonth({
     Key? key,
     required this.startDate,
     required this.finishDate,
-    this.heightCell = 16.0,
+    required this.input,
+    required this.colorThresholds,
+    this.monthsLabels = TimeUtils.defaultMonthsLabels,
+    this.weekDaysLabels = TimeUtils.defaultWeekLabels,
+    this.onTapHeatMapDay,
+    this.cellHeight = 16.0,
     this.disableOpacity = 0.5,
     this.mondayFirstDayWeek = true,
+    this.marginHorizontal = 0,
   }) : super(key: key);
 
   @override
@@ -28,82 +45,54 @@ class _HeatMapCalendarMonthState extends State<HeatMapCalendarMonth> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      var cellWidth = constraints.maxWidth / DateTime.daysPerWeek -
+      var cellWidth = (constraints.maxWidth - widget.marginHorizontal) /
+              DateTime.daysPerWeek -
           HeatMapCalendarMonth.margin;
-      var month = _generateMonth(cellWidth);
       return SizedBox(
+        width: constraints.maxWidth - widget.marginHorizontal,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('2021 ${widget.startDate.month}'),
+            Text(
+                '${widget.startDate.year} ${widget.monthsLabels[widget.startDate.month]}'),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(width: cellWidth + 4, child: Center(child: Text('M'))),
-                SizedBox(width: cellWidth + 4, child: Center(child: Text('T'))),
-                SizedBox(width: cellWidth + 4, child: Center(child: Text('W'))),
-                SizedBox(width: cellWidth + 4, child: Center(child: Text('T'))),
-                SizedBox(width: cellWidth + 4, child: Center(child: Text('F'))),
-                SizedBox(width: cellWidth + 4, child: Center(child: Text('S'))),
-                SizedBox(width: cellWidth + 4, child: Center(child: Text('S'))),
+                SizedBox(
+                    width: cellWidth + 4,
+                    child: const Center(child: Text('M'))),
+                SizedBox(
+                    width: cellWidth + 4,
+                    child: const Center(child: Text('T'))),
+                SizedBox(
+                    width: cellWidth + 4,
+                    child: const Center(child: Text('W'))),
+                SizedBox(
+                    width: cellWidth + 4,
+                    child: const Center(child: Text('T'))),
+                SizedBox(
+                    width: cellWidth + 4,
+                    child: const Center(child: Text('F'))),
+                SizedBox(
+                    width: cellWidth + 4,
+                    child: const Center(child: Text('S'))),
+                SizedBox(
+                    width: cellWidth + 4,
+                    child: const Center(child: Text('S'))),
               ],
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: month,
-            )
+            HeatMapMonth(
+              startDate: widget.startDate,
+              finishDate: widget.finishDate,
+              onTapHeatMapDay: widget.onTapHeatMapDay,
+              cellHeight: widget.cellHeight,
+              cellWidth: cellWidth,
+              disableOpacity: widget.disableOpacity,
+            ),
           ],
         ),
       );
     });
-  }
-
-  List<Row> _generateMonth(double cellWidth) {
-    final firstDayMonth =
-        DateTime(widget.startDate.year, widget.startDate.month, 1);
-    var firstDayWeek = firstDayMonth.weekday;
-
-    var currentDay = DateTime(widget.startDate.year, widget.startDate.month, 1)
-        .subtract(Duration(days: firstDayWeek - 1));
-    final lastDayMonth =
-        DateTime(firstDayMonth.year, firstDayMonth.month + 1, 1)
-            .subtract(const Duration(days: 1));
-
-    bool continueAddDay = true;
-    var month = <Row>[];
-    var week = <HeatMapDay>[];
-    do {
-      var heatmapDay = HeatMapDay(
-        currentDate: currentDay,
-        width: cellWidth,
-        height: widget.heightCell,
-        opacity: _getOpacity(currentDay),
-      );
-      week.add(heatmapDay);
-      if (week.length == 7) {
-        var row = Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: week,
-        );
-        month.add(row);
-        week = [];
-      }
-      currentDay = currentDay.add(const Duration(days: 1));
-      if (currentDay.isAfter(lastDayMonth)) {
-        continueAddDay = currentDay.weekday != DateTime.monday;
-      }
-    } while (continueAddDay);
-
-    return month;
-  }
-
-  double _getOpacity(DateTime currentDay) {
-    return currentDay == widget.startDate ||
-            currentDay == widget.finishDate ||
-            (currentDay.isAfter(widget.startDate) &&
-                currentDay.isBefore(widget.finishDate))
-        ? 1
-        : widget.disableOpacity;
   }
 }
--- lib/heatmap_calendar_month/heatmap_month.dart
+++ lib/heatmap_calendar_month/heatmap_month.dart
@@ -1,16 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:heatmap_calendar/heatmap_calendar_month/heatmap_day.dart';
 
 class HeatMapMonth extends StatelessWidget {
-  final DateTime currentDate;
+  final DateTime startDate;
+  final DateTime finishDate;
+  final Function(DateTime)? onTapHeatMapDay;
+
+  final double cellWidth;
+  final double cellHeight;
+  final double disableOpacity;
+
   const HeatMapMonth({
     Key? key,
-    required this.currentDate,
+    required this.startDate,
+    required this.finishDate,
+    required this.onTapHeatMapDay,
+    required this.cellWidth,
+    required this.cellHeight,
+    required this.disableOpacity,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(currentDate.day.toString()),
-    );
+    var month = _generateMonth(cellWidth);
+
+    return Column(children: month);
+  }
+
+  List<Row> _generateMonth(double cellWidth) {
+    final firstDayMonth = DateTime(startDate.year, startDate.month, 1);
+    var firstDayWeek = firstDayMonth.weekday;
+
+    var currentDate = DateTime(startDate.year, startDate.month, 1)
+        .subtract(Duration(days: firstDayWeek - 1));
+    final lastDayMonth =
+        DateTime(firstDayMonth.year, firstDayMonth.month + 1, 1)
+            .subtract(const Duration(days: 1));
+
+    VoidCallback? _callbackSelectDay;
+
+    bool continueAddDay = true;
+    var month = <Row>[];
+    var week = <HeatMapDay>[];
+    do {
+      var nonExistDay =
+          currentDate.isBefore(startDate) || currentDate.isAfter(finishDate);
+      var heatmapDay = HeatMapDay(
+        currentDay: currentDate,
+        onTapCallback: nonExistDay
+            ? null
+            : (callback, date) {
+                _callbackSelectDay?.call();
+                _callbackSelectDay = callback;
+                onTapHeatMapDay?.call(date);
+              },
+        width: cellWidth,
+        height: cellHeight,
+        opacity: _getOpacity(currentDate),
+        thresholds: const <int, Color>{},
+        value: 0,
+        selectColor: Colors.green,
+      );
+      week.add(heatmapDay);
+      if (week.length == 7) {
+        var row = Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: week,
+        );
+        month.add(row);
+        week = [];
+      }
+      currentDate = currentDate.add(const Duration(days: 1));
+      if (currentDate.isAfter(lastDayMonth)) {
+        continueAddDay = currentDate.weekday != DateTime.monday;
+      }
+    } while (continueAddDay);
+
+    return month;
+  }
+
+  double _getOpacity(DateTime currentDay) {
+    return currentDay == startDate ||
+            currentDay == finishDate ||
+            (currentDay.isAfter(startDate) && currentDay.isBefore(finishDate))
+        ? 1
+        : disableOpacity;
   }
 }
