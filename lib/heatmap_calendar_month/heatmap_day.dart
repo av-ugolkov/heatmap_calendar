af33bac230868d38034884e175c7e2b0e901d2fc
av-ugolkov <makedonskiy07@gmail.com>
2021-10-01 09:27:32 +0300

    внешний вид и выбо дат

--- lib/heatmap_calendar_month/heatmap_day.dart
+++ lib/heatmap_calendar_month/heatmap_day.dart
@@ -1,33 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:heatmap_calendar/heatmap_day.dart';
 
-class HeatMapDay extends StatelessWidget {
-  final DateTime currentDate;
+class HeatMapDay extends StatefulWidget {
+  final DateTime currentDay;
+  final int value;
+  final Map<int, Color> thresholds;
+  final Color activeColor;
+  final Color disabledColor;
+  final Color selectColor;
   final double width;
   final double height;
   final double opacity;
+  final TapHeatMapDayCallback? onTapCallback;
 
-  const HeatMapDay({
-    Key? key,
-    required this.currentDate,
-    required this.width,
-    required this.height,
-    required this.opacity,
-  }) : super(key: key);
+  const HeatMapDay(
+      {Key? key,
+      required this.currentDay,
+      required this.value,
+      required this.width,
+      required this.height,
+      required this.opacity,
+      required this.thresholds,
+      required this.selectColor,
+      this.activeColor = Colors.grey,
+      this.disabledColor = Colors.black12,
+      this.onTapCallback})
+      : super(key: key);
+
+  @override
+  State<HeatMapDay> createState() => _HeatMapDayState();
+}
+
+class _HeatMapDayState extends State<HeatMapDay>
+    with AutomaticKeepAliveClientMixin {
+  bool _isSelect = false;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  Color _getColorFromThreshold() {
+    Color color = widget.activeColor;
+    widget.thresholds.forEach((mapKey, mapColor) {
+      if (widget.value >= mapKey) {
+        color = mapColor;
+      }
+    });
+
+    return color;
+  }
+
+  void deselect() {
+    setState(() {
+      _isSelect = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Opacity(
-      opacity: opacity,
-      child: Container(
-        alignment: Alignment.center,
-        height: height,
-        margin: const EdgeInsets.all(2.0),
+    super.build(context);
+    return GestureDetector(
+      onTap: () {
+        if (widget.onTapCallback != null) {
+          widget.onTapCallback?.call(deselect, widget.currentDay);
+          setState(() {
+            _isSelect = true;
+          });
+        }
+      },
+      child: Opacity(
+        opacity: widget.opacity,
         child: Container(
           alignment: Alignment.center,
-          width: width,
-          color: Colors.grey,
-          child: Center(
-            child: Text(currentDate.day.toString()),
+          width: widget.width + 4,
+          height: widget.height + 4,
+          color: _isSelect ? widget.selectColor : Colors.white,
+          child: Container(
+            alignment: Alignment.center,
+            width: widget.width,
+            height: widget.height,
+            color: widget.onTapCallback == null
+                ? widget.disabledColor
+                : _getColorFromThreshold(),
+            child: Center(
+              child: Text(widget.currentDay.day.toString()),
+            ),
           ),
         ),
       ),
