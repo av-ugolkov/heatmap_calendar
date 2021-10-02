b226323ab621f13ae2157d45f8a13a6ecc2d9942
av-ugolkov <makedonskiy07@gmail.com>
2021-10-02 17:17:55 +0300

    скролл и выделение дат

--- lib/heatmap_calendar_month/heatmap_month.dart
+++ lib/heatmap_calendar_month/heatmap_month.dart
@@ -4,45 +4,50 @@ import 'package:heatmap_calendar/heatmap_calendar_month/heatmap_day.dart';
 class HeatMapMonth extends StatelessWidget {
   final DateTime startDate;
   final DateTime finishDate;
+  final int addCountMonth;
   final Function(DateTime)? onTapHeatMapDay;
 
   final double cellWidth;
   final double cellHeight;
-  final double disableOpacity;
+  final double opacityDisable;
+  final double opacityDayOutOfMonth;
 
   const HeatMapMonth({
     Key? key,
     required this.startDate,
     required this.finishDate,
+    required this.addCountMonth,
     required this.onTapHeatMapDay,
     required this.cellWidth,
     required this.cellHeight,
-    required this.disableOpacity,
+    required this.opacityDisable,
+    required this.opacityDayOutOfMonth,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var month = _generateMonth(cellWidth);
+    var firstDateMonth =
+        DateUtils.addMonthsToMonthDate(startDate, addCountMonth);
+    var lastDateMonth = DateTime(firstDateMonth.year, firstDateMonth.month,
+        DateUtils.getDaysInMonth(firstDateMonth.year, firstDateMonth.month));
+    var month = _generateMonth(firstDateMonth, lastDateMonth, cellWidth);
 
     return Column(children: month);
   }
 
-  List<Row> _generateMonth(double cellWidth) {
-    final firstDayMonth = DateTime(startDate.year, startDate.month, 1);
+  List<Row> _generateMonth(
+      DateTime firstDate, DateTime lastDate, double cellWidth) {
+    final firstDayMonth = DateTime(firstDate.year, firstDate.month, 1);
     var firstDayWeek = firstDayMonth.weekday;
 
-    var currentDate = DateTime(startDate.year, startDate.month, 1)
+    var currentDate = DateTime(firstDate.year, firstDate.month, 1)
         .subtract(Duration(days: firstDayWeek - 1));
-    final lastDayMonth =
-        DateTime(firstDayMonth.year, firstDayMonth.month + 1, 1)
-            .subtract(const Duration(days: 1));
 
     VoidCallback? _callbackSelectDay;
 
-    bool continueAddDay = true;
     var month = <Row>[];
     var week = <HeatMapDay>[];
-    do {
+    for (var i = 0; i < 35; ++i) {
       var nonExistDay =
           currentDate.isBefore(startDate) || currentDate.isAfter(finishDate);
       var heatmapDay = HeatMapDay(
@@ -56,7 +61,7 @@ class HeatMapMonth extends StatelessWidget {
               },
         width: cellWidth,
         height: cellHeight,
-        opacity: _getOpacity(currentDate),
+        opacity: _getOpacity(currentDate, firstDate, lastDate),
         thresholds: const <int, Color>{},
         value: 0,
         selectColor: Colors.green,
@@ -71,19 +76,18 @@ class HeatMapMonth extends StatelessWidget {
         week = [];
       }
       currentDate = currentDate.add(const Duration(days: 1));
-      if (currentDate.isAfter(lastDayMonth)) {
-        continueAddDay = currentDate.weekday != DateTime.monday;
-      }
-    } while (continueAddDay);
-
+    }
     return month;
   }
 
-  double _getOpacity(DateTime currentDay) {
-    return currentDay == startDate ||
-            currentDay == finishDate ||
-            (currentDay.isAfter(startDate) && currentDay.isBefore(finishDate))
-        ? 1
-        : disableOpacity;
+  double _getOpacity(
+      DateTime currentDay, DateTime firstDate, DateTime lastDate) {
+    if (currentDay.isBefore(startDate) || currentDay.isAfter(finishDate)) {
+      return opacityDisable;
+    }
+    if (currentDay.isBefore(firstDate) || currentDay.isAfter(lastDate)) {
+      return opacityDayOutOfMonth;
+    }
+    return 1;
   }
 }
